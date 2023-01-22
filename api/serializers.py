3d904dd8d6f266094479883c88f118d47a67b47b
Vanshul Choudhary <75979288+Cvanshulll@users.noreply.github.com>
2023-01-22 23:48:30 +0530

    Delete api directory

--- api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from api.models import Opportunity
-# Create your serializers here.
-class OpportunitySerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        company_id  = serializers.ReadOnlyField()
-        model = Opportunity
-        fields = "__all__"
-        # fields = ('opportunity_id', 'name', 'location', 'role', 'description', 'start_date', 'date', 'added_date', 'active', 'image', 'created_at', 'updated_at')
--- api/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from api.models import Opportunity
-from api.serializers import OpportunitySerializer
-# Create your views here.
-class OpportunityViewSet(viewsets.ModelViewSet):
-    queryset = Opportunity.objects.all()
-    serializer_class = OpportunitySerializer
